db44fc156884d1263564f67c728ce3aad0b8ebd2
Grant Kelly <gkwrcc@dri.edu>
2014-11-04 14:46:54 -0800

    Use index.html again. Added BigVideo to use as background video.

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,19 +6,76 @@ define([
   'app/router', // Request app/router.js
   'modernizr',
   'videojs',
+  'slimscroll',
+  'easings',
   'fullpage',
-  'slimscroll'
+  'imagesloaded',
+  'bigvideo'
 ],
-function($, _, bootstrap, Backbone, Router, Modernizr, videojs, fullpage, slimscroll) {
+function($, _, bootstrap, Backbone, Router, Modernizr, videojs, slimscroll, easings, fullpage, imagesloaded, bigvideo) {
   var initialize = function(){
     //console.log("in app.initialize");
     // Pass in our Router module and call it's initialize function
-    Router.initialize();
+    //Router.initialize();
 
     // Enable all tooltips.
-    $(function () { $("[data-toggle='tooltip']").tooltip(); });
+    // $(function () { $("[data-toggle='tooltip']").tooltip(); });
     // Enable all popovers.
-    $(function () { $("[data-toggle='popover']").popover(); });
+    // $(function () { $("[data-toggle='popover']").popover(); });
+    // Utility function to start playing a video.
+    function play_background_video(url) {
+        var options = {
+            ambient: true,
+            doLoop: true
+        };
+        if (!window.Modernizr.touch) {
+            // Dont show videos on mobile devices that don't allow autoplay.
+            window.BV.show(url, options);
+        }
+    }
+    function play_video(id) {
+        var vid = document.getElementById(id);
+        if (vid !== null) {
+            if (!vid.played.length && !vid.ended) {
+                window.BV.getPlayer().pause(); // First stop background video.
+                vid.play();
+            }
+        }
+    }
+
+    // Initialize BigVideo
+    window.BV = new $.BigVideo();
+    window.BV.init();
+
+    // Initialize fullpage.js
+    $('#fullpage').fullpage({
+        //Navigation Options
+        menu: '#menu',
+        anchors:['top', 'intro-video', 'about',
+        'ch1-1', 'ch1-2', 'ch1-3', 'ch1-4', 'ch1-5', 'ch1-6', 'ch1-7', 'ch1-8', 'ch1-9', 'ch1-10',
+        'ch2-1'],
+
+        keyboardScrolling: true,
+        animateAnchor: true,
+
+        // Called after a section is loaded.
+        afterLoad: function(anchorLink, index){
+            if(anchorLink == 'intro-video') {
+                play_video("id-video-PNWCG");
+            }
+
+            if(anchorLink == 'ch1-1') {
+                play_background_video("videos/resilient-landscapes-SD.mp4");
+            }
+
+            if(anchorLink == 'ch1-10') {
+                play_video("id-video-wild-weather");
+            }
+        }, // end afterLoad
+        onLeave: function(index, nextIndex, direction) {
+        } // end onLeave
+
+    });
   };
 
   return { 
--- javascripts/main.js
+++ javascripts/main.js
@@ -6,8 +6,16 @@ require.config({
         backbone:'libs/backbone/backbone',
         // jQuery
         jquery:'libs/jquery/jquery-1.11.1/jquery',
+        // jQuery UI
+        'jquery-ui':'libs/jquery-ui.min',
+        // imagesloaded 
+        imagesloaded:'libs/imagesloaded',
+        // BigVideo 
+        bigvideo:'libs/bigvideo',
         // fullpage.js slimscroll jquery plugin
         slimscroll: 'libs/jquery.slimscroll.min',
+        // fullpage.js slimscroll jquery plugin
+        easings: 'libs/jquery.easings.min',
         // fullpage.js jquery plugin
         fullpage: 'libs/jquery.fullPage',
         // bootstrap
@@ -20,12 +28,27 @@ require.config({
         text: 'libs/require/plugins/text'
     },
     shim:{
+        jquery: {
+            exports: 'jQuery'
+        },
         bootstrap:{
             deps:['jquery']
         },
         backbone:{
             deps:['underscore', 'jquery'],
             exports:'Backbone'
+        },
+        slimscroll: {
+            deps: ['jquery']
+        },
+        easings: {
+            deps: ['jquery']
+        },
+        fullpage:{
+            deps:['jquery', 'slimscroll', 'easings']
+        },
+        bigvideo:{
+            deps:['jquery', 'jquery-ui', 'videojs', 'imagesloaded']
         },
         underscore:{
             exports:'_'
