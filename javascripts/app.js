d1f611da9ef4a5364f42d4215ad0e00d9a647c6f
Grant Kelly <gkwrcc@dri.edu>
2014-11-05 16:18:03 -0800

    Added some content for Chapter 1.

--- javascripts/app.js
+++ javascripts/app.js
@@ -60,7 +60,7 @@ function($, _, bootstrap, Backbone, Router, Modernizr, videojs, slimscroll, easi
         //Navigation Options
         menu: '#menu',
         anchors:['top', 'intro-video', 'about',
-        'ch1-1',
+        'ch1-1', 'ch1-2', 'ch1-3',
         'ch2-1', 'ch2-2', 'ch2-3', 'ch2-4', 'ch2-5', 'ch2-6', 'ch2-7', 'ch2-8', 'ch2-9', 'ch2-10',
         'ch3-1',
         'ch4-1',
@@ -78,6 +78,10 @@ function($, _, bootstrap, Backbone, Router, Modernizr, videojs, slimscroll, easi
         afterLoad: function(anchorLink, index){
             if(anchorLink == 'intro-video') {
                 play_video("id-video-PNWCG");
+            }
+
+            if(anchorLink.substring(0,3) == 'ch1') {
+                play_background_video("videos/prelude-SD.mp4");
             }
 
             if(anchorLink.substring(0,3) == 'ch2') {
